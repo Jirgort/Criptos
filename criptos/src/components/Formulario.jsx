88116cb8ab1a988c847ecc909bb680fd872f30c3
Jirgort <Jirgort@gmail.com>
2023-11-16 17:45:42 -0600

    Consulta de API

--- criptos/src/components/Formulario.jsx
+++ criptos/src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useEffect,React} from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
 import{monedas} from '../data/monedas'
@@ -13,6 +13,7 @@ const InputSubmit = styled.input`
     font-size: 20px ;
     border-radius: 5px;
     transition: background-color .3s ease;
+    margin-top: 30px;
     &:hover{
         background-color: #7A7DFE;
         cursor: pointer;
@@ -23,12 +24,21 @@ const InputSubmit = styled.input`
 const Formulario = () => {
     
 
-    const [SelectMonedas]=useSelectMonedas('Elige tu Moneda', monedas)
-    //const [SelectCriptomonedas]=useSelectMonedas('Elige tu Criptomoneda')
-    SelectMonedas()
+    const [moneda,SelectMonedas]=useSelectMonedas('Elige tu Moneda', monedas)
+   useEffect(()=>{
+    const consultarAPI=async()=>{
+      const url="https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD"
+      const respuesta=await fetch(url)
+      const resultado= await respuesta.json()
+      console.log(resultado.Data)
+    }
+    consultarAPI()
+
+   },[])
   return (
     <form>
         <SelectMonedas/>
+       
         
       <InputSubmit type="submit" value="Cotizar" />
     </form>
